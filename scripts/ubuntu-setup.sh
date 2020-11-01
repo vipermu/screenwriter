98041e8f1923d20b002ee95ccb333fc570a9c634
Victor <vipermu97@gmail.com>
2020-11-01 23:33:11 +0100

    style: remove ";"

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -4,35 +4,35 @@ sudo apt install nvidia-driver-390
 # DOCKER INSTALLATION
 # source: https://docs.docker.com/engine/install/ubuntu/
 
-sudo apt-get update;
+sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common;
+    software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # The output should be 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88
-sudo apt-key fingerprint 0EBFCD88;
+sudo apt-key fingerprint 0EBFCD88
 
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable";
+   stable"
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io;
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 # Check if it's working
-sudo docker run hello-world;
+sudo docker run hello-world
 
 
 # DOCKER-COMPOSE INSTALLATION
 # source: https://docs.docker.com/compose/install/
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
-sudo chmod +x /usr/local/bin/docker-compose;
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose;
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Check if it worked
-docker-compose --version;
+docker-compose --version
