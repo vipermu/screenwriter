cb5ba89c80c2a724a1630f1aadeb7c75f08906d8
Victor <vipermu97@gmail.com>
2020-11-28 16:20:20 +0100

    feat: create common, train and generate args

--- screenwriter/args.py
+++ screenwriter/args.py
@@ -3,15 +3,44 @@ import argparse
 from str2bool import str2bool
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-
+def get_common_args(parser: argparse.ArgumentParser,):
     parser.add_argument(
         "--model_name",
         type=str,
         default="gpt2-xl",
         help="Name of the model that we want to use.",
     )
+    parser.add_argument(
+        "--block_size",
+        type=int,
+        default=256,
+        help="Size of each sentence parsed by the model at each "
+        "iteration.",
+    )
+    parser.add_argument(
+        "--recompute_data",
+        type=str2bool,
+        nargs='?',
+        const=True,
+        default=False,
+        help="If True, training data is recomputed",
+    )
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        default=(""),
+        help="Prompt to evaluate results.",
+    )
+
+    args = parser.parse_args()
+
+    return parser, args
+
+
+def get_train_args():
+    parser = argparse.ArgumentParser()
+    parser, _args = get_common_args(parser)
+
     parser.add_argument(
         "--log_dir",
         type=str,
@@ -35,13 +64,6 @@ def get_args():
         type=int,
         default=100,
         help="Number of training epochs.",
-    )
-    parser.add_argument(
-        "--block_size",
-        type=int,
-        default=256,
-        help="Size of each sentence parsed by the model at each "
-        "iteration.",
     )
     parser.add_argument(
         "--learning_rate",
@@ -91,19 +113,46 @@ def get_args():
         default=5000,
         help="Iters frequency at which models are stored.",
     )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def get_generate_args():
+    parser = argparse.ArgumentParser()
+    parser, _args = get_common_args(parser)
+    
     parser.add_argument(
-        "--recompute_data",
-        type=str2bool,
-        nargs='?',
-        const=True,
-        default=False,
-        help="If True, training data is recomputed",
+        "--max_context_len",
+        type=int,
+        default=256,
+        help="Context to take into account for each generation.",
     )
     parser.add_argument(
-        "--prompt",
+        "--max_generation_len",
+        type=int,
+        default=4048,
+        help="Maximum number of tokens that can be generated.",
+    )
+    parser.add_argument(
+        "--output_file",
         type=str,
-        default=(""),
-        help="Prompt to evaluate results.",
+        default="./screenwriter/generation.txt",
+        help="File where the results will be stored.",
+    )
+    parser.add_argument(
+        "--k",
+        type=int,
+        default=50,
+        help="Number of max probability tokens to take into account for each "
+        "prediction.",
+    )
+    parser.add_argument(
+        "--p",
+        type=float,
+        default=0.95,
+        help="Percentile where the selected tokens will be sampled from.",
     )
 
     args = parser.parse_args()
